ed2db415491b580b93f60179934bb5b337298b91
jtimberman <joshua@opscode.com>
2013-05-27 19:07:17 -0600

    release v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## v0.1.2:
+
+### Bug
+
+- [COOK-2964]: jetty cookbook has foodcritic failures
+
 ## v0.1.0:
 
 * [COOK-1434] - Add cargo remote deploy support
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures jetty"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.2"
 
 %w{ java jpackage openssl }.each do |cb|
   depends cb
